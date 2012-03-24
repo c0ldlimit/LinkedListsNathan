eda1999200c56fc4414e3394219d74f3bcd14118
cmai <c0ldlimit@gmail.com>
2012-03-24 18:51:31 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+void insert(Node* head, int data)
+{
+    Node* oldNext = head->next;
+    head->next = new Node;
+    (*(*head).next).data = data;
+    (*(*head).next).next = oldNext;
+}
+
+// wouldn't we need to reference multiple nodes
+// to remove a node in the middle of the list?
+Node* remove(Node* head)
+{
+    Node* newHead = head->next;
+    delete head;
+    return newHead;
+}
+
+void remove_all(Node* head)
+{
+    Node* currentNode = head;
+    while(currentNode!=NULL)
+    {
+        Node* delNode = currentNode;
+        delete delNode;
+        currentNode = currentNode->next;
+    }
+}
+
+Node* find(Node*head,int data)
+{
+    Node* currentNode = head;
+    while(currentNode!=NULL)
+    {
+        if (currentNode->data == data)
+        {
+            break;
+        }
+        else
+        {
+            currentNode=currentNode->next;
+        }
+    }
+    return currentNode;
+}
+
+// I think you need at least 3 nodes to reverse a linked list
+Node* reverse(Node* head)
+{
+    Node* currentNode = head;
+    Node* nextNode = head->next;
+    head->next = NULL;
+    Node* temp;
+    while(nextNode!=NULL)
+    {
+        temp = nextNode->next;
+        nextNode->next = currentNode;
+        currentNode = nextNode;
+        nextNode = temp;
+    }
+    return currentNode;
+}
+
+
+void printList(Node* head)
+{
+    Node* currentNode = head;
+    while(currentNode!=NULL)
+    {
+        cout << currentNode->data << endl;
+        currentNode=currentNode->next;
+    }
+}
+
+int main()
+{
+    Node head;
+    head.data = 5;
+    head.next = NULL;
+
+    // INSERT
+    insert(&head,10);
+    insert((head.next),20);
+    printList(&head); // 5 10 20
+
+    // REMOVE
+    Node* newHead = remove(&head);
+    printList(newHead); // 10 20
+
+    // REMOVE ALL
+    remove_all(newHead);
+
+    // FIND
+    Node headA;
+    headA.data = 5;
+    headA.next = NULL;
+    insert(&headA,10);
+    insert((headA.next),20);
+    Node* foundNode = find(&headA,20);
+    printList(foundNode); // 20
+
+    // REVERSE
+    insert((headA.next),15);
+    Node* reverseHead;
+    printList(&headA);
+    reverseHead = reverse(&headA);
+    printList(reverseHead);
+
+}
